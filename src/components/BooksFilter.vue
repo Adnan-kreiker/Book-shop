cf07443220793aab1ce6316a9c516a2cdb287630
akreiker_aedifion <akreiker@aedifion.com>
2024-05-03 10:48:08 +0200

    Add BooksFilter component

---
+++ src/components/BooksFilter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const selectedPublisher = defineModel({
+  type: String,
+  default: '-',
+})
+
+const props = defineProps<{
+  bookPublishers: string[]
+}>()
+</script>
+
+<template>
+  <form action="#" class="column column-33">
+    <label for="by-publisher">Filter by Publisher</label>
+    <select v-model="selectedPublisher" id="by-publisher">
+      <option value="-">-</option>
+      <option v-for="publisher in props.bookPublishers" :value="publisher" :key="publisher">{{ publisher }}</option>
+    </select>
+  </form>
+</template>
