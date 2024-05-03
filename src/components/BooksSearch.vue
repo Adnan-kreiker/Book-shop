3fa72b2dcadba15902977d572d92342cb35dd423
akreiker_aedifion <akreiker@aedifion.com>
2024-05-03 10:48:18 +0200

    Add BooksSearch component

---
+++ src/components/BooksSearch.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const searchByBookTitle = defineModel({
+  type: String,
+  default: '',
+})
+</script>
+
+<template>
+  <form action="#" class="column">
+    <label for="search">Search by Title</label>
+    <input v-model="searchByBookTitle" type="text" id="search" />
+  </form>
+</template>
