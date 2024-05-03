709b88dcd84dd7f2af274e3c965329bc9cf069bc
akreiker_aedifion <akreiker@aedifion.com>
2024-05-03 13:40:30 +0200

    Export routes for tests

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,30 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 
+export const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    children: [
+      {
+        path: '/favorites',
+        name: 'favorites',
+        component: () => import('../views/FavoritesView.vue')
+      }
+    ]
+  },
+  {
+    path: '/details/:isbn',
+    name: 'details',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/DetailsView.vue')
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      children: [
-        {
-          path: '/favorites',
-          name: 'favorites',
-          component: () => import('../views/FavoritesView.vue')
-        }
-      ]
-    },
-    {
-      path: '/details/:isbn',
-      name: 'details',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/DetailsView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
