b634d027292523fda45466a3f930931ec050fd2f
akreiker_aedifion <akreiker@aedifion.com>
2024-05-03 12:20:09 +0200

    Add Navigation component and integrate it into the App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppNavigation from '@/components/AppNavigation.vue'
 </script>
 
 <template>
@@ -7,16 +8,11 @@ import { RouterLink, RouterView } from 'vue-router'
     <header class="header">
       <div class="container">
         <div class="row">
-          <div class="column"><p class="logo">IT.Book.Library</p></div>
           <div class="column">
-            <nav class="mainnav">
-              <a href="index.html" class="mainnav-link mainnav-link--active"
-                >Books</a
-              >
-              <a href="favorite.html" class="mainnav-link"
-                >Favorites <span class="mainnav-number">2</span></a
-              >
-            </nav>
+            <p class="logo">IT.Book.Library</p>
+          </div>
+          <div class="column">
+            <AppNavigation />
           </div>
         </div>
       </div>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useBooksStore } from '@/stores/books'
+import { storeToRefs } from 'pinia';
+
+const bookStore = useBooksStore()
+
+const { favoriteBooks } = storeToRefs(bookStore)
+</script>
+
+<template>
+  <nav class="mainnav">
+    <RouterLink to="/" class="mainnav-link" exact-active-class="mainnav-link--active">Books</RouterLink>
+    <RouterLink to="/favorites" class="mainnav-link" exact-active-class="mainnav-link--active">Favorites <span
+        class="mainnav-number">{{ favoriteBooks.length
+        }}</span></RouterLink>
+  </nav>
+</template>
