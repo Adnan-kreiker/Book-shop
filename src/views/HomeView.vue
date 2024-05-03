aa1d29bb1e75e996d209e27ac20af69a817f69c4
akreiker_aedifion <akreiker@aedifion.com>
2024-05-03 12:22:31 +0200

    Extend the HomeView to use it as a FavoriteView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,62 @@
 <script setup lang="ts">
-import { useBookStore } from '@/stores/books'
+import { useBooksStore } from '@/stores/books'
 import { storeToRefs } from 'pinia';
 import BooksTable from '@/components/BooksTable.vue';
 import BooksSearch from '@/components/BooksSearch.vue';
 import BooksFilter from '@/components/BooksFilter.vue';
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 
-const bookStore = useBookStore()
+const bookStore = useBooksStore()
 
 bookStore.getBooks()
 
-const { filteredBooks, bookPublishers, selectedPublisher, searchByBookTitle } = storeToRefs(bookStore)
+const { filteredBooks, bookPublishers, selectedPublisher, searchByBookTitle, numberOfFilteredBooks } = storeToRefs(bookStore)
+
+const router = useRouter()
+
+function handleNavigateToDetail(isbn: string) {
+  bookStore.selectedBookISBN = isbn
+
+  router.push({ name: 'details', params: { isbn } })
+}
+
+const booksToDisplay = computed(() => {
+  if (!isFavoritePage.value) {
+    return filteredBooks.value
+  }
+
+  return bookStore.filteredFavoriteBooks
+})
+
+const pageHeaderText = computed(() => {
+  if (!isFavoritePage.value) {
+    if (numberOfFilteredBooks.value === 1) return '1 Book displayed'
+
+    return `${numberOfFilteredBooks.value} Books displayed`
+  }
+
+  if (bookStore.favoriteBooks.length === 1) return '1 Favorite on your list'
+
+  return `${bookStore.favoriteBooks.length} Favorites on your list`
+})
+
+const isFavoritePage = computed(() => router.currentRoute.value.name === 'favorites')
 
 </script>
 
 <template>
   <div class="container">
-    <h2>2 Books displayed</h2>
+    <RouterView />
+    <h2>{{ pageHeaderText }}</h2>
     <section class="row filter-search">
       <BooksSearch v-model="searchByBookTitle" />
       <BooksFilter v-model="selectedPublisher" :bookPublishers />
     </section>
     <section class="row">
       <div class="column">
-        <BooksTable @toggleFavorite="bookStore.toggleFavorite" :books="filteredBooks"
-          :isLoading="bookStore.isLoadingBooks" />
+        <BooksTable @toggleFavorite="bookStore.toggleFavorite" @navigate-to-detail="handleNavigateToDetail"
+          :books="booksToDisplay" :isLoading="bookStore.isLoadingBooks" />
       </div>
     </section>
   </div>
